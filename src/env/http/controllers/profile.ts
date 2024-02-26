5f158310f9faed8064a85fcd49a91e11a6de799c
EZEQUIEL PENAQUIONI <zeca.penaq11@gmailcom>
2024-02-26 09:36:50 -0300

    SubError

--- src/env/http/controllers/profile.ts
+++ src/env/http/controllers/profile.ts
@@ -1,8 +1,24 @@
 import {FastifyRequest,FastifyReply} from 'fastify'
+import { makeGetUserProfileUseCase } from '../services/factories/make-get-user-profile-use-case'
+
 
 
 export  async function profile (request:FastifyRequest,reply:FastifyReply){
    
-     await request.jwtVerify()
-            return reply.status(200).send()
+ 
+
+     const getUserProfile=makeGetUserProfileUseCase()
+     const  {user} = await getUserProfile.execute({
+          userID: request.user.sub
+     
+        
+     })
+     console.log(request.user.sub)
+
+            return reply.status(200).send({
+               user:{
+                    ...user,
+                    password_hash:undefined,
+               }
+            })
 }
--- src/env/http/controllers/routes.ts
+++ src/env/http/controllers/routes.ts
@@ -2,9 +2,10 @@ import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { profile } from "./profile"; 
+import { verifyJwt } from "../middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register);
     app.post('/sessions', authenticate);
-    app.get('/me', profile);
+    app.get('/me', {onRequest:[verifyJwt]} ,profile);
 }
